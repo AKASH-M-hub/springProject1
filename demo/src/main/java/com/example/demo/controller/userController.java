99292a7ea11454c5c00f0fb494b285ca7e8507d3
Akash M <akashmohanraj333@gmail.com>
2026-07-24 11:46:10 +0530

    Initial Commit

---
+++ demo/src/main/java/com/example/demo/controller/userController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.entity.user;
+import com.example.demo.service.JwtService;
+import com.example.demo.service.userService;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class userController {
+
+    private final userService service;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public userController(userService service,
+                          AuthenticationManager authenticationManager,
+                          JwtService jwtService) {
+
+        this.service = service;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+    }
+
+    // ==========================
+    // Register User
+    // ==========================
+    @PostMapping("/register")
+    public user registerUser(@RequestBody user users) {
+        return service.registerUser(users);
+    }
+
+    // ==========================
+    // Login User
+    // ==========================
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+
+        Authentication authentication =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                request.getEmail(),
+                                request.getPassword()
+                        )
+                );
+
+        UserDetails userDetails =
+                (UserDetails) authentication.getPrincipal();
+
+        return jwtService.generateToken(userDetails);
+    }
+}
---
+++ demo/src/main/java/com/example/demo/service/userService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+import com.example.demo.entity.user;
+import com.example.demo.repository.userRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class userService {
+    private userRepository repository;
+    private PasswordEncoder encoder;
+    
+    public userService(userRepository repository, PasswordEncoder encoder) {
+        this.repository = repository;
+        this.encoder = encoder;
+    }
+    public user registerUser(user users)
+    {
+        users.setPassword(encoder.encode(users.getPassword()));
+        return repository.save(users);
+    }
+}
